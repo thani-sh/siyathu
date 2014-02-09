30eb02aff82fc8dc0949014e58fa695ca2efb811
thilina227 <thilina.227@gmail.com>
2014-02-09 16:44:58 +0530

    add popover
    add popover and firebase

--- site_assets/scripts.js
+++ site_assets/scripts.js
@@ -1,129 +1,113 @@
-(function(){
+(function() {
 
+	window.L = function() {
+		if(window.L.enabled)
+			console.log(arguments);
+	}
 
-    window.L = function () {
-        if(window.L.enabled)
-            console.log(arguments);
-    }
+	window.L.enabled = true;
 
-    window.L.enabled = true;
+	// Scripts to run when page is ready
 
+	$(function() {
+		L(':: [window] Page Loaded and Ready');
+	})
+	// Initialize Siyathu App
 
+	window.siyathu = angular.module('siyathu', ['ngRoute']);
 
-    // Scripts to run when page is ready
+	// Initialzie Siyathu Routes
 
-    $(function(){
-        L(':: [window] Page Loaded and Ready');
-    })
+	window.siyathu.config(function($routeProvider) {
 
+		$routeProvider.when('/homepage', {
+			templateUrl : 'site_views/website/homepage.html',
+			controller : 'StaticController'
+		});
 
+		$routeProvider.when('/error/invalid-url', {
+			templateUrl : 'site_views/errors/invalid-url.html',
+			controller : 'StaticController'
+		});
 
-    // Initialize Siyathu App
+		$routeProvider.when('/error', {
+			templateUrl : 'site_views/errors/unknown.html',
+			controller : 'StaticController'
+		});
 
-    window.siyathu = angular.module('siyathu', ['ngRoute']);
+		$routeProvider.when('/:channelId/', {
+			templateUrl : 'site_views/channel/viewer.html',
+			controller : 'ViewerController'
+		});
 
+		$routeProvider.when('/:channelId/editor', {
+			templateUrl : 'site_views/channel/editor.html',
+			controller : 'EditorController'
+		});
 
+		$routeProvider.when('/:channelId/publisher', {
+			templateUrl : 'site_views/channel/publisher.html',
+			controller : 'PublisherController'
+		});
 
-    // Initialzie Siyathu Routes
+		$routeProvider.otherwise({
+			redirectTo : '/homepage'
+		});
 
-    window.siyathu.config(function ($routeProvider) {
+	});
+	// MainController
+	// @TODO Description
 
-        $routeProvider.when('/homepage', {
-            templateUrl:'site_views/website/homepage.html'
-            ,controller:'StaticController'
-        });
+	window.siyathu.controller('MainController', function($scope, $route, $routeParams) {
+		L(':: [controller] MainController');
+		L(JSON.stringify($routeParams));
+	});
+	// StaticController
+	// @TODO Description
 
-        $routeProvider.when('/error/invalid-url', {
-            templateUrl:'site_views/errors/invalid-url.html'
-            ,controller:'StaticController'
-        });
+	window.siyathu.controller('ViewerController', function($scope, $routeParams) {
+		L(':: [controller] ViewerController');
+		L(JSON.stringify($routeParams));
+	});
+	// EditorController
+	// @TODO Description
 
-        $routeProvider.when('/error', {
-            templateUrl:'site_views/errors/unknown.html'
-            ,controller:'StaticController'
-        });
-
-        $routeProvider.when('/:channelId/', {
-            templateUrl:'site_views/channel/viewer.html'
-            ,controller:'ViewerController'
-        });
-
-        $routeProvider.when('/:channelId/editor', {
-            templateUrl:'site_views/channel/editor.html'
-            ,controller:'EditorController'
-        });
-
-        $routeProvider.when('/:channelId/publisher', {
-            templateUrl:'site_views/channel/publisher.html'
-            ,controller:'PublisherController'
-        });
-
-        $routeProvider.otherwise({
-            redirectTo: '/homepage'
-        });
-
-    });
-
-
-
-    // MainController
-    // @TODO Description
-
-    window.siyathu.controller('MainController', function ($scope, $route, $routeParams) {
-        L(':: [controller] MainController');
-        L(JSON.stringify($routeParams));
-    });
-
-
-
-    // StaticController
-    // @TODO Description
-
-    window.siyathu.controller('ViewerController', function ($scope, $routeParams) {
-        L(':: [controller] ViewerController');
-        L(JSON.stringify($routeParams));
-    });
-
-
-
-    // EditorController
-    // @TODO Description
-
-    window.siyathu.controller('EditorController', function ($scope, $routeParams) {
-        L(':: [controller] EditorController');
-        var channelRef = new Firebase('https://ss14-team-140.firebaseio.com/channels/'+$routeParams.channelId);
+	window.siyathu.controller('EditorController', function($scope, $routeParams) {
+		L(':: [controller] EditorController');
+		var channelRef = new Firebase('https://ss14-team-140.firebaseio.com/channels/' + $routeParams.channelId);
 		channelRef.onDisconnect().remove();
-		channelRef.set({foo:'bar'});
+		channelRef.set({
+			foo : 'bar'
+		});
 		$("#carousel1").owlCarousel({
 			pagination : false
 		});
 		$("#carousel2").owlCarousel({
 			pagination : false
 		});
-    });
+		$('.bootstrap-popover').popover({
+			placement : 'top',
+			trigger : 'hover',
+			delay : {
+				show : 10,
+				hide : 1000
+			}
+		});
+	});
+	// PublisherController
+	// @TODO Description
 
+	window.siyathu.controller('PublisherController', function($scope, $routeParams) {
+		L(':: [controller] PublisherController');
+		L(JSON.stringify($routeParams));
+	});
+	// StaticController
+	// @TODO Description
 
-
-    // PublisherController
-    // @TODO Description
-
-    window.siyathu.controller('PublisherController', function ($scope, $routeParams) {
-        L(':: [controller] PublisherController');
-        L(JSON.stringify($routeParams));
-    });
-
-
-
-    // StaticController
-    // @TODO Description
-
-    window.siyathu.controller('StaticController', function ($scope, $routeParams) {
-        L(':: [controller] StaticController');
-        L(JSON.stringify($routeParams));
-    });
-
-
-
-    // EOF
+	window.siyathu.controller('StaticController', function($scope, $routeParams) {
+		L(':: [controller] StaticController');
+		L(JSON.stringify($routeParams));
+	});
+	// EOF
 
 })();
