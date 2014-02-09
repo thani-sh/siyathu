cf56117ac831083bcc175fc0e511d8300fdd0c32
Muhammed Thanish <mnmtanish@gmail.com>
2014-02-09 10:31:56 +0530

    Seperate homepage and other pages

--- site_assets/scripts.js
+++ site_assets/scripts.js
@@ -59,7 +59,7 @@
         });
 
         $routeProvider.otherwise({
-            redirectTo: '/homepage'
+            redirectTo: '/error/invalid-url'
         });
 
     });
@@ -112,7 +112,6 @@
     window.siyathu.controller('StaticController', function ($scope, $routeParams) {
         L(':: [controller] StaticController');
         L(JSON.stringify($routeParams));
-        !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');
     });
 
 
