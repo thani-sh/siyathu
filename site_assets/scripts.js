b9f534d98aebfce6ba72eeb7e95452f63d2deaaa
Muhammed Thanish <mnmtanish@gmail.com>
2014-02-10 02:27:52 +0530

    Add basic video mixer functions

--- site_assets/scripts.js
+++ site_assets/scripts.js
@@ -1,6 +1,9 @@
 (function(){
 
 
+    var SIGNALLING_FIREBASE_NAME = 'ss14-team-140';
+
+
     window.L = function () {
         if(window.L.enabled)
             console.log(arguments);
@@ -81,7 +84,33 @@
 
     window.siyathu.controller('ViewerController', function ($scope, $routeParams) {
         L(':: [controller] ViewerController');
-        L(JSON.stringify($routeParams));
+        var channel = ($routeParams.channelId || 'defaultChannel') + '::' + 'editor<->viewer';
+        var meeting = new Meeting(channel);
+        var remoteVids = document.getElementById('viewer-remote-videos');
+
+        meeting.firebase = SIGNALLING_FIREBASE_NAME;
+
+        meeting.onaddstream = function(e) {
+            L('[stream] Added on E<->V Channel', e);
+            if (e.type == 'local') {
+                // No need to show local stream.
+                // @TODO Stop local media stream
+                e.stream.stop();
+            } else if (e.type == 'remote') {
+                remoteVids.innerHTML = '';
+                remoteVids.appendChild(e.video);
+            }
+        };
+
+        meeting.onuserleft = function(userid) {
+            var video = document.getElementById(userid);
+            if (video) video.parentNode.removeChild(video);
+        };
+
+        meeting.check();
+
+        window.meeting_v = meeting;
+
     });
 
 
@@ -91,6 +120,85 @@
 
     window.siyathu.controller('EditorController', function ($scope, $routeParams) {
         L(':: [controller] EditorController');
+
+        window.streams = {};
+        window.currentStreamId = null;
+        window.previewVideoElement = document.getElementById('editor-preview-video');
+        window._temp_channel = ($routeParams.channelId || 'defaultChannel') + '::' + 'editor<->publisher';
+
+        (function(){
+
+            var channel = ($routeParams.channelId || 'defaultChannel') + '::' + 'editor<->publisher';
+            var meeting = new Meeting(channel);
+            var remoteVids = document.getElementById('editor-remote-videos');
+
+            meeting.firebase = SIGNALLING_FIREBASE_NAME;
+
+            meeting.onaddstream = function(e) {
+                if (e.type == 'local') {
+                    // No need to show local stream.
+                    // @TODO Stop local media stream
+                    e.stream.stop();
+                } else if (e.type == 'remote') {
+                    e.video.classList.add('vid');
+                    //remoteVids.insertBefore(e.video, remoteVids.firstChild);
+                    $("#carousel1").data('owlCarousel').addItem(e.video);
+                    e.video.play();
+                    e.video.onclick = function () {
+                        window.changeStream(e.video.id);
+                    }
+                    window.streams[e.video.id] = e.stream;
+                    if (window.currentStreamId === null) {
+                        forwardStream(e.stream);
+                        window.currentStreamId = e.video.id;
+                        window.previewVideoElement.src = e.video.src;
+                        window.previewVideoElement.play();
+                    }
+                }
+            };
+
+            meeting.onuserleft = function(userid) {
+                var video = document.getElementById(userid);
+                if (video) video.parentNode.removeChild(video);
+            };
+
+            meeting.host(channel);
+
+            window.meeting_e = meeting;
+
+        })();
+
+        function forwardStream (stream) {
+
+            var channel = ($routeParams.channelId || 'defaultChannel') + '::' + 'editor<->viewer';
+            var meeting = new Meeting(channel);
+
+            meeting.firebase = SIGNALLING_FIREBASE_NAME;
+
+            meeting.onaddstream = function(e) {
+                L('[stream] Added on E<->V Channel', e);
+                if (e.type == 'local') {
+                    // No need to show local stream.
+                    // @TODO Stop local media stream
+                    // try using e.stream.stop()
+                } else if (e.type == 'remote') {
+                    //
+                }
+            };
+
+            meeting.forward(channel, stream);
+
+            window.meeting_ep = meeting;
+
+        }
+
+        window.changeStream = function (streamId) {
+            var video = document.getElementById(streamId);
+            window.previewVideoElement.src = video.src;
+            forwardStream(window.streams[streamId]);
+        }
+
+        window.forwardStream = forwardStream;
 
         (function(){
             var channelRef = new Firebase('https://ss14-team-140.firebaseio.com/channels/' + $routeParams.channelId);
@@ -107,8 +215,8 @@
             	 delay : {show : 10, hide: 10}
            	});
         })();
-		
-		
+
+
 		$('#carousel1 .vid').click(function(){
 			alert("change the video stream");
 		});
@@ -124,7 +232,31 @@
 
     window.siyathu.controller('PublisherController', function ($scope, $routeParams) {
         L(':: [controller] PublisherController');
-        L(JSON.stringify($routeParams));
+        var channel = ($routeParams.channelId || 'defaultChannel') + '::' + 'editor<->publisher';
+        var meeting = new Meeting(channel);
+        var localVid = document.getElementById('editor-local-video');
+
+        meeting.firebase = SIGNALLING_FIREBASE_NAME;
+
+        meeting.onaddstream = function(e) {
+            if (e.type == 'local') {
+                localVid.insertBefore(e.video, localVid.firstChild);
+            } else if (e.type == 'remote') {
+                // @TODO No need for remote video preview
+            }
+        };
+
+        meeting.onuserleft = function(userid) {
+            var video = document.getElementById(userid);
+            if (video) video.parentNode.removeChild(video);
+        };
+
+        // @TODO Many things can go wrong here.
+        // Check if channel is available, re-connect option, and more are needed
+        meeting.check();
+
+        window.meeting_p = meeting;
+
     });
 
 
